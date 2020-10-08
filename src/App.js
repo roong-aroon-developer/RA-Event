e69b2d63622677df585bf96684fb85d9cc2da247
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2020-10-08 21:00:26 +0700

    :hammer: fix routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import { Route, BrowserRouter } from 'react-router-dom'
+import React from "react";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
 
-import Login from './pages/Login'
-import Home from './pages/Home'
+import Login from "./pages/Login";
+import Home from "./pages/Home";
 
-const App = ()=> {
+const App = () => {
   return (
     <BrowserRouter>
-      <Route path="/login" component={Login} />
-      <Route path="/" component={Home} />
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/" component={Home} />
+      </Switch>
     </BrowserRouter>
   );
-}
+};
 export default App;
