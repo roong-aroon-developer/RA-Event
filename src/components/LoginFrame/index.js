4ba96af7e0e16494be3780188dabbd89323da598
NaratGutz <naratgutz@gmail.com>
2020-10-08 15:56:42 +0700

    Need to fix position
    Input box position is broken

--- src/components/LoginFrame/index.js
+++ src/components/LoginFrame/index.js
@@ -4,9 +4,9 @@ import './LoginFrame.css'
 const LoginFrame = () => {
     return(
         <div id="loginframe">
-            <input type="text" class = "inputbox" id="username" placeholder="username..."/>
-            <input type="password" class = "inputbox" id="password" placeholder="password..."/>
-            <button id="submitcircle"><img src="./pics/SubmitArrow.png" alt="white arrow" width="29px" id="submitarrow"/></button>
+            <input type="text" className = "inputbox" id="username" placeholder="username..."/>
+            <input type="password" className = "inputbox" id="password" placeholder="password..."/>
+            <button className="submitcircle"><img src="./pics/SubmitArrow.png" alt="white arrow" width="29px" id="submitarrow"/></button>
         </div>
     );
 }
