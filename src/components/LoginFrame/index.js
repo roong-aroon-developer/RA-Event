707417add5858936ee83933817ceb1a342e186d5
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2020-10-08 16:15:49 +0700

    :sparkles: fix bugs on css

--- src/components/LoginFrame/index.js
+++ src/components/LoginFrame/index.js
@@ -4,8 +4,10 @@ import './LoginFrame.css'
 const LoginFrame = () => {
     return(
         <div id="loginframe">
+            <div id="field-wrapper">
             <input type="text" className = "inputbox" id="username" placeholder="username..."/>
             <input type="password" className = "inputbox" id="password" placeholder="password..."/>
+            </div>
             <button className="submitcircle"><img src="./pics/SubmitArrow.png" alt="white arrow" width="29px" id="submitarrow"/></button>
         </div>
     );
