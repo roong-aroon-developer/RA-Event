036f6ebae1447157a8da1ff9e246588dc4240332
NaratGutz <naratgutz@gmail.com>
2020-10-08 15:07:13 +0700

    Zero

---
+++ src/components/LoginText/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './LoginText.css'
+
+const LoginText = () => {
+    return(
+        <div id = "logintext">
+            <div id = "loginword">Login</div>
+            <div id = "loginsubtext">with student ID</div>
+        </div>
+    );
+}
+
+export default LoginText;
