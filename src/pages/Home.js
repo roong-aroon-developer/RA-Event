da933fa59e2bc95074bf03f70cd75303c5e285d5
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2020-10-19 22:14:44 +0700

    :wrench: fix styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 import EventFeed from "../components/EventFeed";
 import GreetUser from "../components/GreetUser";
 import WeekFeed from "..//components/WeekFeed"
