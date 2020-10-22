8f978a20853a06f4acba7a8d30184f37caeed466
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2020-10-22 15:32:08 +0700

    :sparkles: add path for event feed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Route, BrowserRouter, Switch } from "react-router-dom";
 
+import { EventData } from "./store/EventFeedMock";
+
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Info from "./pages/Info";
@@ -9,9 +11,12 @@ const App = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path = "/" component={ Home } />
-        <Route path = "/login" component={ Login } />
-        <Route path = "/test" component={ Info } />
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/test" component={Info} />
+        {EventData.map((data) => (
+          <Route path={data.path} component={Info} />
+        ))}
       </Switch>
     </BrowserRouter>
   );
--- src/store/EventFeedMock.js
+++ src/store/EventFeedMock.js
@@ -1,20 +1,26 @@
 export const EventData = [
-    {
-        name: "Hello World1",
-        desc: "It's the most famous program.",
-        path: "/test"
-    },
-    {
-        name: "Hello World2",
-        desc: "It's the most famous program.",
-        path: "/test2"
-    },
-    {
-        name: "Hello World3",
-        desc: "It's the most famous program.",
-        path: "/test3"},
-    {
-        name: "Hello World4",
-        desc: "It's the most famous program.",
-        path: "/test4"},
-]
+  {
+    name: "Hello World1",
+    desc: "It's the most famous program.",
+    path: "/test",
+    component: "Info1",
+  },
+  {
+    name: "Hello World2",
+    desc: "It's the most famous program.",
+    path: "/test2",
+    component: "Info1",
+  },
+  {
+    name: "Hello World3",
+    desc: "It's the most famous program.",
+    path: "/test3",
+    component: "Info1",
+  },
+  {
+    name: "Hello World4",
+    desc: "It's the most famous program.",
+    path: "/test4",
+    component: "Info1",
+  },
+];
