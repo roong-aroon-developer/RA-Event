a650853ec9392f4a3ab03ebd52f0b62f709ddaf9
NaratGutz <naratgutz@gmail.com>
2020-10-27 16:08:09 +0700

    Added spacing to carousel. Add more formatting to Info

--- src/components/EventPage/index.jsx
+++ src/components/EventPage/index.jsx
@@ -1,12 +1,14 @@
 import React, { Fragment } from 'react'
 import EventImage from './EventImage'
 import EventTag from './EventTag'
+import EventContent from './EventContent'
 
 const EventPage = (props) => {
     return(
         <Fragment>
             <EventImage/>
             <EventTag/>
+            <EventContent />
         </Fragment>
     )
 }
