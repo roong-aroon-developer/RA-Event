6a4e33ac7a48c58b36fff073a0d6cb59f8609720
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2020-10-29 15:03:03 +0700

    :sparkles: feat - navbar animations

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -7,26 +7,40 @@ import { makeStyles } from "@material-ui/core/styles";
 import BottomNavigation from "@material-ui/core/BottomNavigation";
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
 
+import "./Navbar.css";
+
 const useStyles = makeStyles({
   root: {
     width: "95%",
     height: 60,
     position: "fixed",
     bottom: 12,
+    left: "2.5%",
     backgroundColor: "#ffffff",
     borderRadius: 30,
     zIndex: 1,
     boxShadow: "1px 3px 7px #00000024",
     margin: "auto",
-    left: "50%",
-    transform: "translate(-50%, 0)",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
 });
 
 const Navbar = () => {
   const classes = useStyles();
+  const [show, setShow] = React.useState(true);
+  const [scrollPos, setScrollPos] = React.useState(true);
+  const handleScroll = () => {
+    setScrollPos(document.body.getBoundingClientRect().top);
+    setShow(document.body.getBoundingClientRect().top > scrollPos);
+  };
+  React.useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  });
   return (
-    <BottomNavigation className={classes.root}>
+    <BottomNavigation className={classes.root} id={show ? "active" : "hidden"}>
       <BottomNavigationAction
         showLabel={true}
         label="Home"
--- src/store/weekFeedMock.js
+++ src/store/weekFeedMock.js
@@ -22,5 +22,51 @@ export const weekData = [
         desc: "It's the most famous program. ... As a function, the computer program simply tells the computer to display the words “Hello, World!” Traditionally, it's the first program developers use to test systems.",
         path: "/week4",
         component: "Info",
+    },{
+        name: "Hello World1",
+        desc: "It's the most famous program. ... As a function, the computer program simply tells the computer to display the words “Hello, World!” Traditionally, it's the first program developers use to test systems.",
+        path: "/week",
+        component: "Info",
+    },
+    {
+        name: "Hello World2",
+        desc: "It's the most famous program. ... As a function, the computer program simply tells the computer to display the words “Hello, World!” Traditionally, it's the first program developers use to test systems.",
+        path: "/week2",
+        component: "Info",
+    },
+    {
+        name: "Hello World3",
+        desc: "It's the most famous program. ... As a function, the computer program simply tells the computer to display the words “Hello, World!” Traditionally, it's the first program developers use to test systems.",
+        path: "/week3",
+        component: "Info",
+    },
+    {
+        name: "Hello World4",
+        desc: "It's the most famous program. ... As a function, the computer program simply tells the computer to display the words “Hello, World!” Traditionally, it's the first program developers use to test systems.",
+        path: "/week4",
+        component: "Info",
+    },{
+        name: "Hello World1",
+        desc: "It's the most famous program. ... As a function, the computer program simply tells the computer to display the words “Hello, World!” Traditionally, it's the first program developers use to test systems.",
+        path: "/week",
+        component: "Info",
+    },
+    {
+        name: "Hello World2",
+        desc: "It's the most famous program. ... As a function, the computer program simply tells the computer to display the words “Hello, World!” Traditionally, it's the first program developers use to test systems.",
+        path: "/week2",
+        component: "Info",
+    },
+    {
+        name: "Hello World3",
+        desc: "It's the most famous program. ... As a function, the computer program simply tells the computer to display the words “Hello, World!” Traditionally, it's the first program developers use to test systems.",
+        path: "/week3",
+        component: "Info",
+    },
+    {
+        name: "Hello World4",
+        desc: "It's the most famous program. ... As a function, the computer program simply tells the computer to display the words “Hello, World!” Traditionally, it's the first program developers use to test systems.",
+        path: "/week4",
+        component: "Info",
     },
 ]
