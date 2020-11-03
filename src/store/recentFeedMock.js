3f0f4903e7fa877ef6fc702506609615e5783c64
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2020-11-03 21:54:54 +0700

    :sparkles: feat -  Use same format for mockup data

--- src/store/recentFeedMock.js
+++ src/store/recentFeedMock.js
@@ -3,6 +3,7 @@ export const recentData = [
     name: "วันเฉลิมพระชนมพรรษาพระบาทสมเด็จพระวชิรเกล้าเจ้าอยู่หัว",
     path: "/test",
     tags: "news",
+    date: "21 Sept 2020",
     img: "https://dummyimage.com/340x180",
     imgAlt: "ในหลวง",
     body: "This is body"
@@ -11,6 +12,7 @@ export const recentData = [
     name: "วันเฉลิมพระชนมพรรษาพระบาทสมเด็จพระวชิรเกล้าเจ้าอยู่หัว",
     path: "/test2",
     tags: "news",
+    date: "21 Sept 2020",
     img: "https://dummyimage.com/340x180",
     imgAlt: "ในหลวง",
     body: "This is body"
@@ -19,6 +21,7 @@ export const recentData = [
     name: "วันเฉลิมพระชนมพรรษาพระบาทสมเด็จพระวชิรเกล้าเจ้าอยู่หัว",
     path: "/test3",
     tags: "news",
+    date: "21 Sept 2020",
     img: "https://dummyimage.com/340x180",
     imgAlt: "ในหลวง",
     body: "This is body"
@@ -27,6 +30,7 @@ export const recentData = [
     name: "วันเฉลิมพระชนมพรรษาพระบาทสมเด็จพระวชิรเกล้าเจ้าอยู่หัว",
     path: "/test4",
     tags: "news",
+    date: "21 Sept 2020",
     img: "https://dummyimage.com/340x180",
     imgAlt: "ในหลวง",
     body: "This is body"
--- src/store/weekFeedMock.js
+++ src/store/weekFeedMock.js
@@ -1,23 +1,29 @@
 export const weekData = [
     {
         name: "10 ข้อปฏิบัติควรรู้ก่อนเปิดภาคเรียนแรกในปี 2020",
-        date: "21 Sept 2020",
-        tag: "news",
         path: "/week",
-        component: "Info"
+        tag: "news",
+        date: "21 Sept 2020",
+        img: "",
+        imgAlt: "",
+        body: ""
     },
     {
         name: "หน่วยบริการปีการศึกษา ๑/๒๕๖๓ เปิดให้ลงทะเบียนแล้ว",
-        date: "21 Sept 2020",
-        tag: "registration",
         path: "/week2",
-        component: "Info"
+        tag: "registration",
+        date: "21 Sept 2020",
+        img: "",
+        imgAlt: "",
+        body: ""
     },
     {
         name: "เปิดรับแล้ว! ศึกษาดูงานกับ KMUTT Formula Student",
-        date: "21 Sept 2020",
-        tag: "news",
         path: "/week3",
-        component: "Info"
+        tag: "news",
+        date: "21 Sept 2020",
+        img: "",
+        imgAlt: "",
+        body: ""
     }
 ]
