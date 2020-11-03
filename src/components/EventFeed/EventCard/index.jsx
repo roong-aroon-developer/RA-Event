bed414b1b0057aade8aa19b4ade61767122c95c4
NaratGutz <naratgutz@gmail.com>
2020-11-03 13:38:40 +0700

    delete @ibm/plex

--- src/components/EventFeed/EventCard/index.jsx
+++ src/components/EventFeed/EventCard/index.jsx
@@ -3,8 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 
-import '@ibm/plex';
-
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
