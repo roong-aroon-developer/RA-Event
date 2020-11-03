c365ca98db1af54ddfd0b7dd31361bc5d93b275f
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2020-11-03 15:06:26 +0700

    :sparkles: feat - update mockup info

--- src/store/EventFeedMock.js
+++ src/store/EventFeedMock.js
@@ -2,21 +2,30 @@ export const EventData = [
   {
     name: "วันเฉลิมพระชนมพรรษาพระบาทสมเด็จพระวชิรเกล้าเจ้าอยู่หัว",
     path: "/test",
-    component: "Info1",
+    tags: ["news", "registration"],
+    img: "",
+    body: ""
+
   },
   {
     name: "วันเฉลิมพระชนมพรรษาพระบาทสมเด็จพระวชิรเกล้าเจ้าอยู่หัว",
     path: "/test2",
-    component: "Info1",
+    tags: ["news", "registration"],
+    img: "",
+    body: ""
   },
   {
     name: "วันเฉลิมพระชนมพรรษาพระบาทสมเด็จพระวชิรเกล้าเจ้าอยู่หัว",
     path: "/test3",
-    component: "Info1",
+    tags: ["news", "registration"],
+    img: "",
+    body: ""
   },
   {
     name: "วันเฉลิมพระชนมพรรษาพระบาทสมเด็จพระวชิรเกล้าเจ้าอยู่หัว",
     path: "/test4",
-    component: "Info1",
+    tags: ["news", "registration"],
+    img: "",
+    body: ""
   },
 ];
