c937ad03c42741eac0732010f236ea166a9bf9f0
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2020-11-03 14:21:06 +0700

    :sparkles: feat - add LoginPage

---
+++ src/components/ProfilePage/index.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+import Avatar from '@material-ui/core/Avatar';
+
+const ProfilePage = () => {
+    return(
+        <Fragment>
+            <Avatar alt="profile" src="https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/studio77-mckinsey-6653-pai_2_1.jpg?bg=transparent&con=3&cs=srgb&dpr=1&fm=jpg&ixlib=php-3.1.0&q=65&usm=15&vib=3&w=600&s=99b44ef23a4b1adc068f123672212bd1"/>
+        </Fragment>
+    )
+}
+
+export default ProfilePage
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Login from "../components/Login"
+import ProfilePage from "../components/ProfilePage"
 
 const Profile = () => {
   return (
-    <Login />
+    <Login/>
   )
 }
 
