02207b88ec4e84d93731425c84a9bb93f41fbc1e
NaratGutz <naratgutz@gmail.com>
2020-11-10 16:17:12 +0700

    :sparkles: Added buttons to profile page

--- src/components/ProfilePage/index.jsx
+++ src/components/ProfilePage/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Avatar from "@material-ui/core/Avatar";
 import TextField from "@material-ui/core/TextField";
 import FormControl from '@material-ui/core/FormControl';
@@ -12,7 +12,7 @@ const useStyles = makeStyles((theme) => ({
     [`& fieldset`]: {
       borderRadius: 25,
     },
-    width: '60%',
+    width: 'auto',
     marginTop: '3%'
   },
 }));
@@ -20,29 +20,40 @@ const useStyles = makeStyles((theme) => ({
 const ProfilePage = (props) => {
   const classes = useStyles();
   return (
-    <div id="profile-wrapper">
-      <Avatar
-        id="profile-img"
-        alt="profile"
-        src="https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/studio77-mckinsey-6653-pai_2_1.jpg?bg=transparent&con=3&cs=srgb&dpr=1&fm=jpg&ixlib=php-3.1.0&q=65&usm=15&vib=3&w=600&s=99b44ef23a4b1adc068f123672212bd1"
-      />
-      
-        <TextField 
-            className={classes.textField}
-            label="Display name"
-            type="text"
-            variant="outlined"
+      <Fragment>
+        <div id="profile-wrapper">
+        <Avatar
+            id="profile-img"
+            alt="profile"
+            src="https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/studio77-mckinsey-6653-pai_2_1.jpg?bg=transparent&con=3&cs=srgb&dpr=1&fm=jpg&ixlib=php-3.1.0&q=65&usm=15&vib=3&w=600&s=99b44ef23a4b1adc068f123672212bd1"
         />
         
-      <TextField
-        className={classes.textField}
-        label="Class"
-        type="text"
-        variant="outlined"
-        helperText="For example, 5/1"
-        id="Classroom"
-      />
-    </div>
+            <TextField 
+                className={classes.textField}
+                label="Display name"
+                type="text"
+                variant="outlined"
+            />
+            
+        <TextField
+            className={classes.textField}
+            label="Class"
+            type="text"
+            variant="outlined"
+            helperText="For example, 5/1"
+            id="Classroom"
+        />
+        
+            <div id = 'btn-wrapper'>
+                <button id = 'cancel-btn'>
+                    Cancel
+                </button>
+                <button id = 'save-btn'>
+                    Save
+                </button>
+            </div>
+        </div>
+    </Fragment>
   );
 };
 
