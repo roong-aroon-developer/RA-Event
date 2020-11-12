9dd24c9bc603d30b429cded68dd89ba6bd432a0b
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2020-11-12 14:50:54 +0700

    :wrench: fix - header file size

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Mask from '../../static/mask-01.png';
 import Weather from '../../static/weather-icon.png';
+import Skeleton from '@material-ui/lab/Skeleton';
 import './Header.css';
 
 const Header = () => {
@@ -17,11 +18,11 @@ const Header = () => {
                 <div id = "InfoCircle">
                     <div id='aqi-column'>
                         <img src={Mask} alt='mask' id='aqi'/>
-                        <div id='aqi-number'>xx aqi</div>
+                        <div id='aqi-number'><Skeleton width={40} variant="text"/></div>
                     </div>
                     <div id='temperature-column'>
                         <img src={Weather} alt='weather-cloud' id='weather'/>
-                        <div id='temperature'>x°c</div>
+                        <div id='temperature'><Skeleton width={40} variant="text"/></div>
                     </div>
                 </div>
             </div>
