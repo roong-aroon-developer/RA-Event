7c557c532812df024e32f224d6817d626ebdc826
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2020-11-26 14:49:35 +0700

    :sparkles: - fetch recent from database

--- src/components/RecentFeed/index.jsx
+++ src/components/RecentFeed/index.jsx
@@ -1,63 +1,68 @@
-import React, { lazy, Suspense} from 'react'
-import Carousel from 'react-multi-carousel'
-import { Link } from 'react-router-dom'
-import RecentCardSkeleton from './RecentCardSkeleton'
+import React, { lazy, Suspense } from "react";
+import Carousel from "react-multi-carousel";
+import { Link } from "react-router-dom";
+import RecentCardSkeleton from "./RecentCardSkeleton";
 
-import "react-multi-carousel/lib/styles.css"
-import './RecentFeed.css'
+import "react-multi-carousel/lib/styles.css";
+import "./RecentFeed.css";
 
-import { recentData } from '../../store/recentFeedMock'
-const RecentCard = lazy(() => import('./RecentCard'))
+import { recentData } from "../../store/recentFeedMock";
+const RecentCard = lazy(() => import("./RecentCard"));
 
 const responsive = {
-    largeDesktop: {
-      breakpoint: { max: 3000, min: 1280 },
-      items: 3,
-      slidesToSlide: 3, // optional, default to 1.
-    },
-    tablet: {
-      breakpoint: { max: 1280, min: 640 },
-      items: 2,
-      slidesToSlide: 1, // optional, default to 1.
-    },
-    mobile: {
-      breakpoint: { max: 640, min: 0 },
-      items: 1,
-      slidesToSlide: 1, // optional, default to 1.
-    },
-  }
+  largeDesktop: {
+    breakpoint: { max: 3000, min: 1280 },
+    items: 3,
+    slidesToSlide: 3, // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 1280, min: 640 },
+    items: 2,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 640, min: 0 },
+    items: 1,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+};
 
 const RecentFeed = () => {
-    return(
-        <div>
-            <div className='FeedText'>
-            Upcoming Event
-            </div>
-            <Carousel
-                swipeable={true}
-                draggable={true}
-                showDots={true}
-                arrows={false}
-                responsive={responsive}
-                focusOnSelect={true}
-                itemClass="Item"
-              >
-                {
-                    recentData.map(data => (
-                        <Link to={{
-                          pathname: data.path,}}
-                          key={data._id}>
-                            <Suspense fallback={<RecentCardSkeleton/>}>
-                              <RecentCard title={data.name} src={data.img} alt={data.imgAlt}/>
-                            </Suspense>
-                        </Link>
-                    ))
-                } 
-            </Carousel>    
-            
-        </div>
-        
-    );
-}
+  const [Data, setData] = React.useState([])
+  React.useEffect(() => {
+    fetch("http://edu.roong-aroon.ac.th:9000/events")
+      .then((data) => data.json())
+      .then(e => {
+        setData(e.data)
+      })
+  });
+  return (
+    <div>
+      <div className="FeedText">Upcoming Event</div>
+      <Carousel
+        swipeable={true}
+        draggable={true}
+        showDots={true}
+        arrows={false}
+        responsive={responsive}
+        focusOnSelect={true}
+        itemClass="Item"
+      >
+        {Data.map((data) => (
+          <Link
+            to={{
+              pathname: data.path,
+            }}
+            key={data._id}
+          >
+            <Suspense fallback={<RecentCardSkeleton />}>
+              <RecentCard title={data.name} src={data.img} alt={data.imgAlt} />
+            </Suspense>
+          </Link>
+        ))}
+      </Carousel>
+    </div>
+  );
+};
 
 export default RecentFeed;
