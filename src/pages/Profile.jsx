f4c5bdfdec2f26fc94ab70af6680240622a1ce4c
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2020-11-28 13:58:47 +0700

    :sparkles: - redirect after signin

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,11 +2,18 @@ import React, { Fragment } from "react";
 import Login from "../components/Login"
 import ProfilePage from "../components/ProfilePage"
 
+import { AuthContext } from '../store/AuthContext'
 const Profile = () => {
+  const { loggedIn } = React.useContext(AuthContext)
   return (
-    <Fragment>
-      <Login />
-      {/* <ProfilePage /> */}
+   <Fragment> 
+    {
+      loggedIn ? (
+        <ProfilePage />   
+      ) : (
+        <Login />
+      )
+    }
     </Fragment>
   )
 }
