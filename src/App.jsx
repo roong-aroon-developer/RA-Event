857342393c2600a604c53cbb821179b57c950023
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2021-02-09 15:07:56 +0700

    :sparkles: - set development environment 2

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Navbar from "./components/Navbar";
 import Loader from "./components/Loader";
 import Home from "./pages/Home";
 import Unavailable from "./pages/Unavailable"
-import { isMobile } from "react-device-detect";
+//import { isMobile } from "react-device-detect";
 const EventPage = React.lazy(() => import("./components/EventPage"));
 const Profile = React.lazy(() => import("./pages/Profile"));
 const Mail = React.lazy(() => import("./pages/Mail"));
