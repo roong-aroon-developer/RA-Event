34ef7fab0d6f21fbceb07d4e895f2a13b048a084
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2021-02-19 00:03:06 +0700

    :sparkles: - remove unused class

--- src/components/UnavailablePage/index.jsx
+++ src/components/UnavailablePage/index.jsx
@@ -1,21 +1,7 @@
 import React, { Fragment } from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import TextContainer from "./TextContainer"
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        height: "100%",
-        width: "100%",
-    },
-    textstyles: {
-        color: "black",
-        fontSize: 32,
-        zIndex: 999,
-    }
-}));
-
 export default function UnavailablePage(){
-    const classes = useStyles();
     return(
         <Fragment>
              <TextContainer/>
