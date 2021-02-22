6ff0e251283801e00f275b8c9ec6c641119c5983
NaratGutz <naratgutz@gmail.com>
2021-02-22 15:29:59 +0700

    implemented aqi calculation

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,17 +6,36 @@ import "./Header.css";
 import { AuthContext } from "../../store/AuthContext";
 
 const Header = () => {
-  const [pm, setPm] = React.useState(0);
   const [celcius, setCelcius] = React.useState(0);
   const { userInfo } = React.useContext(AuthContext)
   const [displayName, setDisplayName] = React.useState("")
+  const [pmAqi, setPmAqi] = React.useState(0);
   React.useEffect(() => {
     fetch("https://ra-dust-detect.now.sh/")
       .then((data) => data.json())
       .then((data) => {
-        setPm(parseInt(data.pm25));
         setCelcius(parseInt(data.temp));
+        if(parseFloat(data.pm25Avg) <= 12.0){
+          setPmAqi(Math.trunc(((50/12)*parseFloat(data.pm25Avg))*10)/10);
+        }
+        else if(parseFloat(data.pm25Avg) >= 12.1 && parseFloat(data.pm25Avg) <= 35.4){
+          setPmAqi(Math.trunc((((49/23.3)*(parseFloat(data.pm25Avg)-12.1))+51)*10)/10);
+        }
+        else if(parseFloat(data.pm25Avg) >= 35.5 && parseFloat(data.pm25Avg) <= 55.4){
+          setPmAqi(Math.trunc((((49/19.9)*(parseFloat(data.pm25Avg)-35.5))+101)*10)/10);
+        } 
+        else if(parseFloat(data.pm25Avg) >= 55.5 && parseFloat(data.pm25Avg) <= 150.4){
+          setPmAqi(Math.trunc((((49/54.9)*(parseFloat(data.pm25Avg)-55.5))+151)*10)/10);
+        }
+        else if(parseFloat(data.pm25Avg) >= 150.5 && parseFloat(data.pm25Avg) <= 250.4){
+          setPmAqi(Math.trunc((((99/99.9)*(parseFloat(data.pm25Avg)-150.5))+201)*10)/10);
+        }
+        else if(parseFloat(data.pm25Avg) >= 250.5 && parseFloat(data.pm25Avg) <= 500.4){
+          setPmAqi(Math.trunc((((199/249.9)*(parseFloat(data.pm25Avg)-250.5))+301)*10)/10);
+        }
       });
+    
+
     if(userInfo.name) {
       setDisplayName(userInfo.name.split(" ")[0].charAt(0).toUpperCase() + userInfo.name.split(" ")[0].toLowerCase().slice(1))
     }
@@ -33,7 +52,7 @@ const Header = () => {
           <div id="aqi-column">
             <img src={Mask} alt="mask" id="aqi" />
             <div id="aqi-number"> 
-              {pm !== 0 ? pm.toString().concat(" aqi") : <Skeleton width={40} variant="text" />}
+              {pmAqi !== 0 ? pmAqi.toString().concat(" aqi") : <Skeleton width={40} variant="text" />}
             </div>
           </div>
           <div id="temperature-column">
